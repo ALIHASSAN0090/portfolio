415e3fd7fa8368642bd6930be358116466eba737
Ali hassan <alihassankhan285@gmail.com>
2024-03-05 12:29:59 +0500

    Fix unescaped single quote character

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,64 +1,48 @@
-"use client"
 import React from "react";
-import {motion} from "framer-motion";
-import { slideInFromLeft, slideInFromRight ,slideInFromTop } from "@/utils/motion";
+import { motion } from "framer-motion";
+import { slideInFromLeft, slideInFromRight, slideInFromTop } from "@/utils/motion";
 import { SparklesIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
-const HeroContent = () =>{
-
-    return (
-
+const HeroContent = () => {
+  return (
     <motion.div
-    initial = "hidden"
-    animate = "visible"
-    className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
     >
-        <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
-            <motion.div
-            variants={slideInFromTop}
-            className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-            >
-            <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5'/>
-            <h1 className='Welcome-text text-[13px]'>Full Stack Developer</h1>
-            </motion.div>
-
-            <motion.div 
-            variants={slideInFromLeft(0.5)}
-            className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
-            >
-                <span>
-                Hi, it's me
-                
-            <br/>
-                <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> Ali Hassan Zahid</span>
-                <br/>
-                
-                </span>
-            </motion.div>
-            <motion.p
-            variants={slideInFromLeft(0.8)}
-            className="text-lg text-gray-400 my-5 max-w-[600px]"
-            >
-                I am a passionate web developer with eager to make something above your expectations!
-
-            </motion.p>
-            <motion.p 
-            variants={slideInFromLeft(1)}
-            className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
-Download CV
-            </motion.p>
-        </div>
-        <motion.div 
-        variants={slideInFromRight(0.8)}
-        className="w-full h-full flex justify-center items-center">
-<img src="/mainIconsdark.svg" alt="work icons" 
-height={650}
-width={650}
-/>
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <h1 className="Welcome-text text-[13px]">Full Stack Developer</h1>
         </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Hi, it&apos;s me
+            <br />
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> Ali Hassan Zahid</span>
+            <br />
+          </span>
+        </motion.div>
+        <motion.p variants={slideInFromLeft(0.8)} className="text-lg text-gray-400 my-5 max-w-[600px]">
+          I am a passionate web developer with eager to make something above your expectations!
+        </motion.p>
+        <motion.p variants={slideInFromLeft(1)} className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]">
+          Download CV
+        </motion.p>
+      </div>
+      <motion.div variants={slideInFromRight(0.8)} className="w-full h-full flex justify-center items-center">
+        <img src="/mainIconsdark.svg" alt="work icons" height={650} width={650} />
+      </motion.div>
     </motion.div>
-    )
-}
+  );
+};
 
 export default HeroContent;
